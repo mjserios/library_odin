4688b1418f6ecf50db042a1f2482f9b464626287
Mark <mjohnserios@gmail.com>
2024-08-13 22:14:43 +0800

    Complete Tasks 4-6 and CSS

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+//
 const submit = document.getElementById('submit');
+const dialogButton = document.getElementById('openDialog');
+const diaLib = document.getElementById('bookDialog');
+const closeDia = document.getElementById('cancel');
 const myLibrary = [];
 
+//
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,11 +13,13 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+//
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
+//
 function submitButton(event) {
     event.preventDefault();
     title = document.getElementById('title').value;
@@ -24,6 +31,7 @@ function submitButton(event) {
     writeLib();
 }
 
+//
 function writeLib() {
     const grids = document.getElementById('grids');
     grids.innerHTML = '';
@@ -38,11 +46,45 @@ function writeLib() {
         authorTxt.textContent = book.author;
         cards.appendChild(authorTxt);
 
+        var pageTxt = document.createElement('p');
+        pageTxt.textContent = `Pages ${book.pages}`;
+        cards.appendChild(pageTxt);
+
+        var readTxt = document.createElement('p');
+        readTxt.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+        cards.appendChild(readTxt);
+
+        var delButton = document.createElement('button');
+        delButton.textContent = "Remove";
+        delButton.classList.add('delButton');
+        delButton.addEventListener('click', () => {
+            myLibrary.splice(index, 1);
+            writeLib();
+        })
+        cards.appendChild(delButton);
+
+        var readToggle = document.createElement('button');
+        readToggle.textContent = 'Change Read Status';
+        readToggle.classList.add('readToggle');
+        readToggle.addEventListener('click', () => {
+            book.read = !book.read;
+            writeLib();
+        })
+        cards.appendChild(readToggle);
 
     grids.appendChild(cards);
     })
 
 }
+
+dialogButton.addEventListener('click', () => {
+    diaLib.showModal();
+})
+
+closeDia.addEventListener('click', () => {
+    diaLib.close();
+})
+
 
 submit.addEventListener('click', submitButton);
 
